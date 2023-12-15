d284869f49762bf95508cd5ee9ce5c88622d0aa5
Edward Kim <itgma1231@gmail.com>
2023-12-15 13:48:16 -0500

    add star icons for product review

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,27 +1,30 @@
 /* eslint-disable react/prop-types */
-import { Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = (props) => {
   return (
     <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${props._id}`}>
+        <Card.Img src={props.image} variant="top" />
+      </Link>
+
+      <Card.Body>
         <Link to={`/product/${props._id}`}>
-            <Card.Img src={props.image} variant="top" />
+          <Card.Title as="div" className="product-title">
+            <strong>{props.name}</strong>
+          </Card.Title>
         </Link>
 
-        <Card.Body>
-            <Link to={`/product/${props._id}`}>
-                <Card.Title as="div">
-                    <strong>{props.title}</strong>
-                </Card.Title>
-            </Link>
+        <Card.Text as="div">
+          <Rating value={props.rating} text={`${props.numReviews} reviews`} />
+        </Card.Text>
 
-            <Card.Text as="h3">
-                ${props.price}
-            </Card.Text>
-        </Card.Body>
+        <Card.Text as="h3">${props.price}</Card.Text>
+      </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
