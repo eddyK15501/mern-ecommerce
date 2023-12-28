abb37926fb37f05aceedfdb4d08e8e9219b78f5f
Edward Kim <itgma1231@gmail.com>
2023-12-28 11:11:36 -0500

    .eslintrc.cjs fix; redux store initial setup

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,6 +5,8 @@ const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
 });
 
 export default store;
