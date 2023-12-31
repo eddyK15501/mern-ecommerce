4cb7476f23f49a67eb9b1f347d2735afb9525ba7
Edward Kim <itgma1231@gmail.com>
2023-12-31 16:23:22 -0500

    dispatch addToCart() reducer with product as action.payload

--- client/src/pages/ProductScreen.jsx
+++ client/src/pages/ProductScreen.jsx
@@ -1,18 +1,38 @@
-import { Link, useParams } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import {
+  Form,
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useGetProductDetailsQuery } from "../redux/slices/productsApiSlice";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/slices/cartSlice";
 
 const ProductScreen = () => {
+  const [qty, setQty] = useState(1);
   const { id: productId } = useParams();
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const {
     data: product,
     isLoading,
     isError,
   } = useGetProductDetailsQuery(productId);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate("/cart");
+  };
 
   return (
     <>
@@ -66,11 +86,39 @@ const ProductScreen = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row className="d-flex align-items-center">
+                      <Col>Quantity:</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(Number(e.target.value))}
+                          style={{ cursor: "pointer" }}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option
+                              key={x + 1}
+                              value={x + 1}
+                              style={{ cursor: "pointer" }}
+                            >
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
                     className="btn-block"
                     type="button"
                     disabled={product.countInStock === 0}
+                    onClick={addToCartHandler}
                   >
                     Add To Cart
                   </Button>
