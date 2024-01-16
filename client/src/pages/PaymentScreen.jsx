548c571b5a6b34e941415b309e0d7882d8767bc0
Edward Kim <itgma1231@gmail.com>
2024-01-16 15:00:20 -0500

    add payment method to redux store; if shippingAddress, redirect to /shipping or /placeorder

--- client/src/pages/PaymentScreen.jsx
+++ client/src/pages/PaymentScreen.jsx
@@ -1,18 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import CheckoutSteps from "../components/CheckoutSteps";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { savePaymentMethod } from "../redux/slices/cartSlice";
 
 const PaymentScreen = () => {
   const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
 
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 step3 />
       <h1>Payment Method</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group>
-          <Form.Label as="legend">Select Method</Form.Label>
+          <Form.Label className="mt-3 mb-4" as="legend">
+            Select Method
+          </Form.Label>
           <Col>
             <Form.Check
               type="radio"
@@ -26,7 +49,7 @@ const PaymentScreen = () => {
             ></Form.Check>
           </Col>
         </Form.Group>
-        <Button type="submit" variant="primary">
+        <Button className="mt-4" type="submit" variant="primary">
           Continue
         </Button>
       </Form>
