56ad3140386d3472321d157106ece18359f3386c
Edward Kim <itgma1231@gmail.com>
2024-01-21 16:27:05 -0500

    ProfileScreen.jsx import statement setup

--- client/src/pages/ProfileScreen.jsx
+++ client/src/pages/ProfileScreen.jsx
@@ -1,7 +1,24 @@
-const ProfileScreen = () => {
-  return (
-    <div>ProfileScreen</div>
-  )
-}
+import { useState, useEffect } from "react";
+import { Table, Form, Button, Row, Col } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useProfileMutation } from "../redux/slices/usersApiSlice";
+import { setCredentials } from "../redux/slices/authSlice";
 
-export default ProfileScreen
+const ProfileScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return <div>ProfileScreen</div>;
+};
+
+export default ProfileScreen;
