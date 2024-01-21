03069e07cd0e3780cc3004c38996832a20128882
Edward Kim <itgma1231@gmail.com>
2024-01-21 16:51:34 -0500

    Update user profile form completed; first <Col></Col> done

--- client/src/pages/ProfileScreen.jsx
+++ client/src/pages/ProfileScreen.jsx
@@ -18,7 +18,69 @@ const ProfileScreen = () => {
 
   const { userInfo } = useSelector((state) => state.auth);
 
-  return <div>ProfileScreen</div>;
+  useEffect(() => {
+    if (userInfo) {
+      setName(userInfo.name);
+      setEmail(userInfo.email);
+    }
+  }, [userInfo.name, userInfo.email]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit handled");
+  };
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        <Form>
+          <Form.Group controlId="name" className="mt-3 mb-1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email" className="mt-2 mb-1">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="password" className="mt-2 mb-1">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="confirmPassword" className="mt-2 mb-1">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <div className="d-flex justify-content-center">
+            <Button type="submit" variant="primary" className="mt-3 mb-1">
+                Update Profile
+            </Button>
+          </div>
+        </Form>
+      </Col>
+      <Col md={9}>Column</Col>
+    </Row>
+  );
 };
 
 export default ProfileScreen;
