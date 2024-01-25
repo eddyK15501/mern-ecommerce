e750be114403bdd5a2e2c193963d05e335eea641
Edward Kim <itgma1231@gmail.com>
2024-01-24 20:52:53 -0500

    npm i multer; uploadRoutes.js(); router.post() images to the server at /api/upload

---
+++ server/routes/uploadRoutes.js
@@ -0,0 +1,42 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    // cb for "callback"; null is for defining error;
+    // image files will be uploaded to uploads/ folder in root
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Images only!");
+  }
+}
+
+const upload = multer({ storage });
+
+// 'image' is the file.fieldname
+router.post("/", upload.single("image"), (req, res) => {
+  res.send({
+    message: "Image Uploaded",
+    image: `${req.file.path}`,
+  });
+});
+
+export default router;
