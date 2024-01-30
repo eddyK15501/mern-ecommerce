0326be0718b0ebaf5bd4970935fb3b6fd679da58
Edward Kim <itgma1231@gmail.com>
2024-01-30 17:49:35 -0500

    get the three highest rated products; getTopProducts() controller/router

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 import {
   getProducts,
@@ -7,12 +7,14 @@ import {
   updateProduct,
   deleteProduct,
   createProductReview,
-} from "../controllers/productController.js";
-import { protect, admin } from "../middleware/authMiddleware.js";
+  getTopProducts,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.get('/top', getTopProducts);
 router
-  .route("/:id")
+  .route('/:id')
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
