34bd791006b404f5e93740b984b3852271b5fa61
Edward Kim <itgma1231@gmail.com>
2024-01-31 09:17:18 -0500

    npm install react-helmet-async; Meta.jsx for SEO

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/styles/index.scss";
-import HomeScreen from "./pages/HomeScreen.jsx";
-import ProductScreen from "./pages/ProductScreen.jsx";
-import CartScreen from "./pages/CartScreen.jsx";
-import LoginScreen from "./pages/LoginScreen.jsx";
-import RegisterScreen from "./pages/RegisterScreen.jsx";
-import ShippingScreen from "./pages/ShippingScreen.jsx";
-import PaymentScreen from "./pages/PaymentScreen.jsx";
+import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
+import store from './redux/store.js';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.scss';
+import HomeScreen from './pages/HomeScreen.jsx';
+import ProductScreen from './pages/ProductScreen.jsx';
+import CartScreen from './pages/CartScreen.jsx';
+import LoginScreen from './pages/LoginScreen.jsx';
+import RegisterScreen from './pages/RegisterScreen.jsx';
+import ShippingScreen from './pages/ShippingScreen.jsx';
+import PaymentScreen from './pages/PaymentScreen.jsx';
 import PlaceOrderScreen from './pages/PlaceOrderScreen';
 import OrderScreen from './pages/OrderScreen';
-import ProfileScreen from "./pages/ProfileScreen.jsx";
-import OrderListScreen from "./pages/admin/OrderListScreen.jsx";
-import ProductListScreen from "./pages/admin/ProductListScreen.jsx";
-import ProductEditScreen from "./pages/admin/ProductEditScreen.jsx";
+import ProfileScreen from './pages/ProfileScreen.jsx';
+import OrderListScreen from './pages/admin/OrderListScreen.jsx';
+import ProductListScreen from './pages/admin/ProductListScreen.jsx';
+import ProductEditScreen from './pages/admin/ProductEditScreen.jsx';
 import UserListScreen from './pages/admin/UserListScreen';
-import UserEditScreen from "./pages/admin/UserEditScreen.jsx";
+import UserEditScreen from './pages/admin/UserEditScreen.jsx';
 
-import PrivateRoute from "./components/PrivateRoute.jsx";
+import PrivateRoute from './components/PrivateRoute.jsx';
 import AdminRoute from './components/AdminRoute';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
@@ -36,89 +36,89 @@ const router = createBrowserRouter([
         element: <HomeScreen />,
       },
       {
-        path: "/search/:keyword",
+        path: '/search/:keyword',
         element: <HomeScreen />,
       },
       {
-        path: "/page/:pageNumber",
+        path: '/page/:pageNumber',
         element: <HomeScreen />,
       },
       {
-        path: "/search/:keyword/page/:pageNumber",
+        path: '/search/:keyword/page/:pageNumber',
         element: <HomeScreen />,
       },
       {
-        path: "/product/:id",
+        path: '/product/:id',
         element: <ProductScreen />,
       },
       {
-        path: "/cart",
+        path: '/cart',
         element: <CartScreen />,
       },
       {
-        path: "/login",
+        path: '/login',
         element: <LoginScreen />,
       },
       {
-        path: "/register",
+        path: '/register',
         element: <RegisterScreen />,
       },
       // Private routes for logged in authenticated users
       {
-        path: "/shipping",
+        path: '/shipping',
         element: <PrivateRoute component={ShippingScreen} />,
       },
       {
-        path: "/payment",
-        element: <PrivateRoute component={PaymentScreen} />
+        path: '/payment',
+        element: <PrivateRoute component={PaymentScreen} />,
       },
       {
-        path: "/placeorder",
-        element: <PrivateRoute component={PlaceOrderScreen} />
+        path: '/placeorder',
+        element: <PrivateRoute component={PlaceOrderScreen} />,
       },
       {
         path: '/order/:id',
-        element: <PrivateRoute component={OrderScreen} />
+        element: <PrivateRoute component={OrderScreen} />,
       },
       {
         path: '/profile',
-        element: <PrivateRoute component={ProfileScreen} />
+        element: <PrivateRoute component={ProfileScreen} />,
       },
       // Admin routes for admin user only
       {
         path: '/admin/orderlist',
-        element: <AdminRoute component={OrderListScreen} />
+        element: <AdminRoute component={OrderListScreen} />,
       },
       {
         path: '/admin/productlist',
-        element: <AdminRoute component={ProductListScreen} />
+        element: <AdminRoute component={ProductListScreen} />,
       },
       {
         path: '/admin/productlist/:pageNumber',
-        element: <AdminRoute component={ProductListScreen} />
+        element: <AdminRoute component={ProductListScreen} />,
       },
       {
         path: '/admin/product/:id/edit',
-        element: <AdminRoute component={ProductEditScreen} />
+        element: <AdminRoute component={ProductEditScreen} />,
       },
       {
         path: '/admin/userlist',
-        element: <AdminRoute component={UserListScreen} />
+        element: <AdminRoute component={UserListScreen} />,
       },
       {
         path: '/admin/user/:id/edit',
-        element: <AdminRoute component={UserEditScreen} />
-      }
+        element: <AdminRoute component={UserEditScreen} />,
+      },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
     <Provider store={store}>
       <PayPalScriptProvider deferLoading={true}>
         <RouterProvider router={router} />
       </PayPalScriptProvider>
     </Provider>
-  </React.StrictMode>
+  </HelmetProvider>
 );
