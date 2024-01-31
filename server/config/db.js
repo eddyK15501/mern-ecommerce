b990925f1b9c6ff0121b0c6c78e72fbc290d6460
Edward Kim <itgma1231@gmail.com>
2024-01-31 18:16:43 -0500

    process.env.MONGODB_URI to string error fix; deploying to Render

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    const connect = await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (err) {
     console.log(`Error: ${err.message}`);
